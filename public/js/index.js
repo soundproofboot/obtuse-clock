4d577064e874fad2c7fc1fbbfa7fbe642e65a457
C Bares <colinedwinbares@gmail.com>
2022-03-01 14:58:56 -0600

    timeout 939/2 flash cascade

--- public/js/index.js
+++ public/js/index.js
@@ -1,58 +1,75 @@
 let divs = document.querySelectorAll('div');
 
-divs.forEach(div => div.style.opacity = 0);
-// const heading = document.querySelector('h1');
-let timeout = 1000;
+divs.forEach((div) => (div.style.opacity = 0));
+
+let timeout = 939/2;
 
 function callBack() {
   let time = Date.now().toString();
-  
-  let milli = Number(time[time.length - 1])
-  let hundredth = Number(time[time.length-2])
-  let tenth = Number(time[time.length -3]);
-  let second = Number(time[time.length -4]);
+
+  let milli = Number(time[time.length - 1]);
+  let hundredth = Number(time[time.length - 2]);
+  let tenth = Number(time[time.length - 3]);
+  let second = Number(time[time.length - 4]);
 
   let div1 = document.querySelector(`#div${milli}`);
   div1.style.opacity = `.${hundredth}`;
 
   setTimeout(() => {
-    div1.style.opacity = `0`
-  },timeout*hundredth)
+    div1.style.opacity = `0`;
+  }, timeout/2);
 
   let div2 = document.querySelector(`#div1${hundredth}`);
   div2.style.opacity = `.${tenth}`;
 
   setTimeout(() => {
     div2.style.opacity = `.0`;
-  },timeout*tenth)
-
+  }, timeout/4);
 
   let div3 = document.querySelector(`#div2${tenth}`);
   div3.style.opacity = `.${second}`;
 
   setTimeout(() => {
     div3.style.opacity = `.0`;
-  },timeout*second)
+  }, timeout/8);
 
   let div4 = document.querySelector(`#div3${milli}`);
 
   setTimeout(() => {
     div4.style.opacity = `.0`;
-  },timeout*milli)
+  }, timeout/2);
 
   setTimeout(() => {
-    if (timeout >= 150) {
-      timeout -= 10;
-    };
+    // if (timeout >= 150) {
+    //   timeout -= 10;
+    // }
     callBack();
   }, timeout);
-}
-setTimeout(() => {
-  callBack();
-}, 1000)
 
-// const intervalID = setInterval(callBack, 100);
+  
+}
 
 // setTimeout(() => {
-//   clearInterval(intervalID);
-// }, 5000);
+//   callBack();
+// }, timeout);
+
+let startBtn = document.querySelector('button');
+// startBtn.textContent = 'Start';
+
+// div.appendChild(startBtn);
+startBtn.addEventListener('click', () => {
+  var audio = new Audio('./get-away-from-me.mp3');
+  audio.play();
+  // setTimeout(() => {
+    callBack();
+    // }, 10)
+  // setInterval(() => {
+  // divs.forEach((div) => (div.style.opacity = 1));
+
+  // divs.forEach((div) => (div.style.opacity = 0));
+
+  // },timeout*8);
+  startBtn.style.display = 'none';
+  // setInterval(flash, 960);
+  
+});
